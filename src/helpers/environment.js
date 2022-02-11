323d652a253700d0d44ec4f57debfdb9eeaa5ce5
Kyle Ketchmark <kyle.r.ketchmark@gmail.com>
2022-02-11 10:53:43 -0800

    .

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,10 +4,10 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3333';
         break;
-        
-    case 'abk-health-is-wealth-client.herokuapp.com':
-        APIURL = 'https://abk-health-is-wealth.herokuapp.com';
-        break
+
+    case 'krk-nutrition-client.herokuapp.com/':
+        APIURL = 'https://krk-nutrition-tracker.herokuapp.com/';
+        break;
 }
 
 export default APIURL;
